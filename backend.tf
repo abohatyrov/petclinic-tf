d2436bca748f44159a31b0fda61e92ab063f3e97
abohatyrov <bohatyrovandrey@gmail.com>
2023-02-28 11:39:05 +0200

    Updated backend

--- backend.tf
+++ backend.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    google = {
+      version = "~> 3.83.0"
+    }
+  }
+  
   backend "gcs" {
     credentials = "petclinic-app-94cd559f8bb4.json"
     bucket      = "petclinic-tfstate"
--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      version = "~> 3.83.0"
-    }
-  }
-}
-
 provider "google" {
   credentials = file("petclinic-app-94cd559f8bb4.json")
   region      = var.region
